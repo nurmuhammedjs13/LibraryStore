da11867d4ef6a81becd8c5d8e09bd48c88a190b1
nurmuhammedjs13 <nurmuhammedaltybaev@gmail.com>
2024-12-10 15:24:58 +0600

    added

--- src/appPages/site/components/pages/HomePage.tsx
+++ src/appPages/site/components/pages/HomePage.tsx
@@ -2,11 +2,11 @@ import React from "react";
 import Welcome from "./Welcome/Welcome";
 
 const HomePage = () => {
-  return (
-    <div>
-      <Welcome />
-    </div>
-  );
+    return (
+        <div>
+            <Welcome />
+        </div>
+    );
 };
 
 export default HomePage;
