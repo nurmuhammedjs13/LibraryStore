a03eea6ed324f5b9a5fdf14c4ff2c862df16290a
nurgul <NurgulDev>
2025-01-01 15:03:25 +0600

    feat: delivery

---
+++ src/appPages/site/components/pages/Delivery.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import DeliveryInfo from "./deliverySection/DeliveryInfo";
+
+const Delivery = () => {
+  return (
+    <div>
+      <DeliveryInfo />
+    </div>
+  );
+};
+
+export default Delivery;
