595a0174ba03d89642149e3d35231730b6c6485f
shaiyldaevich <salamalikbaryktabasov@gmail.com>
2025-01-19 16:04:31 +0600

    reset and forgot password

--- src/redux/api/auth/types.d.ts
+++ src/redux/api/auth/types.d.ts
@@ -1,72 +1,85 @@
 namespace AUTH {
-    type GetResponse = {
-        id: number;
-        user_image: string;
-        username: string;
-        phone_number: string;
-        email: string;
+  type GetResponse = {
+    id: number;
+    user_image: string;
+    username: string;
+    phone_number: string;
+    email: string;
+  };
+  type GetRequest = void;
+  type PostLoginResponse = {
+    user: {
+      username: string;
+      email: string;
     };
-    type GetRequest = void;
-    type PostLoginResponse = {
-        user: {
-            username: string;
-            email: string;
-        };
-        access: string;
-        refresh: string;
+    access: string;
+    refresh: string;
+  };
+  type PostLoginRequest = {
+    username: string;
+    password: string;
+  };
+  type PostRegistrationResponse = {
+    username: string;
+    email: string;
+    date_registered: string;
+  };
+  type PostRegistrationRequest = {
+    username: string;
+    email: string;
+    password: string;
+  };
+
+  type GetResponse = {
+    id?: number;
+    user_image: string;
+    username: string;
+    phone_number: string;
+    email: string;
+  };
+  type GetRequest = void;
+  type PostLoginResponse = {
+    user: {
+      username: string;
+      email: string;
     };
-    type PostLoginRequest = {
-        username: string;
-        password: string;
-    };
-    type PostRegistrationResponse = {
-        username: string;
-        email: string;
-        date_registered: string;
-        
-    };
-    type PostRegistrationRequest = {
-        username: string;
-        email: string;
-        password: string;
-    };
-   
-    type GetResponse = {
-        id?: number;
-        user_image: string;
-        username: string;
-        phone_number: string;
-        email: string;
-    };
-    type GetRequest = void;
-    type PostLoginResponse = {
-        user: {
-            username: string;
-            email: string;
-        };
-        access: string;
-        refresh: string;
-    };
-    type PostLoginRequest = {
-        username: string;
-        password: string;
-    };
-    type PostRegistrationResponse = {
-        username: string;
-        email: string;
-        date_registered: string;
-      
-    };
-    type PostRegistrationRequest = {
-        username: string;
-        email: string;
-        password: string;
-    };
-    type PostRefreshTokenRes = {
-       access: string;
-       refresh: string;
-    };
-    type PostRefreshTokenReq = {
-        refresh: string;
-    };
+    access: string;
+    refresh: string;
+  };
+  type PostLoginRequest = {
+    username: string;
+    password: string;
+  };
+  type PostRegistrationResponse = {
+    username: string;
+    email: string;
+    date_registered: string;
+  };
+  type PostRegistrationRequest = {
+    username: string;
+    email: string;
+    password: string;
+  };
+  type PostRefreshTokenRes = {
+    access: string;
+    refresh: string;
+  };
+  type PostRefreshTokenReq = {
+    refresh: string;
+  };
+
+  type PostForgotPasswordResponse = {
+    status: string;
+  };
+  type PostForgotPasswordRequest = {
+    email: string;
+  };
+  type PatchResetPasswordResponse = {
+    message: string;
+  };
+  type PatchResetPasswordRequest = {
+    email: string;
+    reset_code: string;
+    new_password: string;
+  };
 }
