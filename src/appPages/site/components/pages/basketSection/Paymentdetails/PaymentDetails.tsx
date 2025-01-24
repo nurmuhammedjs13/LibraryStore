b7154bd3a1f4978db37f2c70f670db68c8ec7ef1
nurmuhammedjs13 <nurmuhammedaltybaev@gmail.com>
2025-01-24 15:07:51 +0600

    added

--- src/appPages/site/components/pages/basketSection/Paymentdetails/PaymentDetails.tsx
+++ src/appPages/site/components/pages/basketSection/Paymentdetails/PaymentDetails.tsx
@@ -6,73 +6,73 @@ import Link from "next/link";
 import { useGetPaymentdetailsQuery } from "@/redux/api/paymentDetails";
 
 const PaymentDetails = () => {
-  const {
-    data,
-    isLoading: isGlobalLoading,
-    isError: isGlobalError,
-  } = useGetPaymentdetailsQuery();
+    const {
+        data,
+        isLoading: isGlobalLoading,
+        isError: isGlobalError,
+    } = useGetPaymentdetailsQuery();
 
-  const {
-    data: paymentDetailData = [],
-    isLoading: isLocalLoading,
-    isError: isLocalError,
-  } = useGetPaymentdetailsQuery(undefined, {
-    selectFromResult: ({ data, isLoading, isError }) => ({
-      data: data || [],
-      isLoading,
-      isError,
-    }),
-  });
+    const {
+        data: paymentDetailData = [],
+        isLoading: isLocalLoading,
+        isError: isLocalError,
+    } = useGetPaymentdetailsQuery(undefined, {
+        selectFromResult: ({ data, isLoading, isError }) => ({
+            data: data || [],
+            isLoading,
+            isError,
+        }),
+    });
 
-  if (isGlobalLoading || isLocalLoading) {
-    return <>Loading...</>;
-  }
+    if (isGlobalLoading || isLocalLoading) {
+        return <>Loading...</>;
+    }
 
-  if (isGlobalError || isLocalError) {
-    return <>Error</>;
-  }
+    if (isGlobalError || isLocalError) {
+        return <>Error</>;
+    }
 
-  return (
-    <div className={styles.detailsMain}>
-      <div className={styles.details}>
-        <div className="container">
-          <div className={styles.breatCrums}>
-            <h3>
-              <Link href="/">Главная </Link>
-              <span>/ Корзина</span>
-            </h3>
-          </div>
-        </div>
-
-        <div className={styles.detailsBlock}>
-          <div className="container">
-            <h2>Реквизиты для оплаты</h2>
-            <div className={styles.chek}>
-              {paymentDetailData.map((el, index) => (
-                <div key={index} className={styles.detailsBox}>
-                  <Image
-                    width={100}
-                    height={17}
-                    className={styles.BankName}
-                    src={el.image_name_bank}
-                    alt="img"
-                  />
-                  <Image
-                    width={100}
-                    height={100}
-                    className={styles.QrCode}
-                    src={el.image_qr_code}
-                    alt="img"
-                  />
-                  <h2>Счет:{el.cart_number}</h2>
+    return (
+        <div className={styles.detailsMain}>
+            <div className={styles.details}>
+                <div className="container">
+                    <div className={styles.breatCrums}>
+                        <h3>
+                            <Link href="/">Главная </Link>
+                            <span>/ Корзина</span>
+                        </h3>
+                    </div>
                 </div>
-              ))}
+
+                <div className={styles.detailsBlock}>
+                    <div className="container">
+                        <h2>Реквизиты для оплаты</h2>
+                        <div className={styles.chek}>
+                            {paymentDetailData.map((el, index) => (
+                                <div key={index} className={styles.detailsBox}>
+                                    <Image
+                                        width={100}
+                                        height={17}
+                                        className={styles.BankName}
+                                        src={el.image_name_bank}
+                                        alt="img"
+                                    />
+                                    <Image
+                                        width={100}
+                                        height={100}
+                                        className={styles.QrCode}
+                                        src={el.image_qr_code}
+                                        alt="img"
+                                    />
+                                    <h2>Счет:{el.cart_number}</h2>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default PaymentDetails;
--- src/appPages/site/components/pages/basketSection/PlacinganOrder/PlacinganOrder.tsx
+++ src/appPages/site/components/pages/basketSection/PlacinganOrder/PlacinganOrder.tsx
@@ -8,138 +8,145 @@ import Minus from "@/assets/Icons/Minus";
 import Plus from "@/assets/Icons/Plus";
 import Price from "@/assets/Icons/Price";
 import ImgStop from "@/assets/Icons/imgStop";
+import { useGetCartItemsQuery } from "@/redux/api/addToCart";
 
 const PlacinganOrder = () => {
-  const landmarkCard = [
-    {
-      img: img,
-      name: "К себе нежно",
-      author: "Ольга Примаченко",
-      date: "30.11.2024",
-      price: 400,
-    },
-  ];
+    const { data = [], isLoading, isError } = useGetCartItemsQuery();
 
-  const [activeButton, setActiveButton] = useState<"delivery" | "pickup">(
-    "delivery"
-  );
-  return (
-    <div className={styles.mainBlock}>
-      <div className="container">
-        <div className={styles.block}>
-          <div className={styles.landmark}>
-            {landmarkCard.map((el, index) => (
-              <div className={styles.cardBlock} key={index}>
-                <div className={styles.ImgBlock}>
-                  <div className={styles.img}>
-                    <Image src={el.img} alt="img" />
-                  </div>
-                  <div className={styles.ImgText}>
-                    <h2>{el.name}</h2>
-                    <h3>{el.author}</h3>
-                    <h4>{el.date}</h4>
-                    <DeleteIcon />
-                  </div>
+    const [activeButton, setActiveButton] = useState<"delivery" | "pickup">(
+        "delivery"
+    );
+    return (
+        <div className={styles.mainBlock}>
+            <div className="container">
+                <div className={styles.block}>
+                    <div className={styles.landmark}>
+                        {data.map((item, index) => (
+                            <div className={styles.cardBlock} key={index}>
+                                <div className={styles.ImgBlock}>
+                                    <div className={styles.img}>
+                                        <Image
+                                            src={
+                                                item.books.book_images[0]
+                                                    .book_images
+                                            }
+                                            alt="img"
+                                            width={100}
+                                            height={100}
+                                        />
+                                    </div>
+                                    <div className={styles.ImgText}>
+                                        <h2>{item.books.book_name}</h2>
+                                        <h3>{item.books.author}</h3>
+                                        <DeleteIcon />
+                                    </div>
+                                </div>
+                                <div className={styles.quantity}>
+                                    <Minus />
+                                    <h1>{item.quantity}</h1>
+                                    <Plus />
+                                </div>
+                                <div className={styles.price}>
+                                    <Price />
+                                    <h5>{item.books.price} сом</h5>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                    <div className={styles.mainInput}>
+                        <h1>Оформление заказа</h1>
+                        <div className={styles.InputBlock}>
+                            <div className={styles.BlockBtn}>
+                                <button
+                                    className={`${styles.delivery} ${
+                                        activeButton === "delivery"
+                                            ? styles.activeDelivery
+                                            : ""
+                                    }`}
+                                    onClick={() => setActiveButton("delivery")}
+                                >
+                                    Доставка
+                                </button>
+                                <button
+                                    className={`${styles.pickup} ${
+                                        activeButton === "pickup"
+                                            ? styles.activePickup
+                                            : ""
+                                    }`}
+                                    onClick={() => setActiveButton("pickup")}
+                                >
+                                    Самовывоз
+                                </button>
+                            </div>
+                            <div className={styles.Inputs}>
+                                <div className={styles.Input}>
+                                    <h6>Имя</h6>
+                                    <input type="text" />
+                                    <hr />
+                                </div>
+                                <div className={styles.Input}>
+                                    <h6>Фамилия</h6>
+                                    <input type="text" />
+                                    <hr />
+                                </div>
+                                <div className={styles.Input}>
+                                    <h6>gmail</h6>
+                                    <input type="text" />
+                                    <hr />
+                                </div>
+                                <div className={styles.Input}>
+                                    <h6>Добавьте номер телефона</h6>
+                                    <input type="number" />
+                                    <hr />
+                                </div>
+                                {activeButton === "delivery" && (
+                                    <div className={styles.Input}>
+                                        <h6>Укажите адрес доставки</h6>
+                                        <input type="text" />
+                                        <hr />
+                                    </div>
+                                )}
+                                <div className={styles.Input}>
+                                    <h6>Комментарии к заказу</h6>
+                                    <input type="text" />
+                                    <hr />
+                                </div>
+                            </div>
+                            <div className={styles.uploadFile}>
+                                <div className={styles.text1}>
+                                    <h4>Товары: 4шт</h4>
+                                    <h4>2800 сом</h4>
+                                </div>
+                                <div className={styles.text2}>
+                                    <h4>
+                                        Цену за доставку обговаривается с
+                                        продавцом
+                                    </h4>
+                                </div>
+                                <div className={styles.text3}>
+                                    <h3>Итого</h3>
+                                    <h3>2800 сом</h3>
+                                </div>
+                                <div className={styles.text4}>
+                                    <h2>Загрузите чек оплаты</h2>
+                                    <div className={styles.text5}>
+                                        <p>Реквизиты в шапке страницы</p>
+                                        <ImgStop />
+                                    </div>
+                                </div>
+                                <div className={styles.upload}>
+                                    <div className={styles.selectFile}>
+                                        <input type="file" />
+                                    </div>
+                                    <button>Оформить заказ</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div className={styles.quantity}>
-                  <Minus />
-                  <h1>0</h1>
-                  <Plus />
-                </div>
-                <div className={styles.price}>
-                  <Price />
-                  <h5>{el.price} сом</h5>
-                </div>
-              </div>
-            ))}
-          </div>
-          <div className={styles.mainInput}>
-            <h1>Оформление заказа</h1>
-            <div className={styles.InputBlock}>
-              <div className={styles.BlockBtn}>
-                <button
-                  className={`${styles.delivery} ${
-                    activeButton === "delivery" ? styles.activeDelivery : ""
-                  }`}
-                  onClick={() => setActiveButton("delivery")}
-                >
-                  Доставка
-                </button>
-                <button
-                  className={`${styles.pickup} ${
-                    activeButton === "pickup" ? styles.activePickup : ""
-                  }`}
-                  onClick={() => setActiveButton("pickup")}
-                >
-                  Самовывоз
-                </button>
-              </div>
-              <div className={styles.Inputs}>
-                <div className={styles.Input}>
-                  <h6>Имя</h6>
-                  <input type="text" />
-                  <hr />
-                </div>
-                <div className={styles.Input}>
-                  <h6>Фамилия</h6>
-                  <input type="text" />
-                  <hr />
-                </div>
-                <div className={styles.Input}>
-                  <h6>gmail</h6>
-                  <input type="text" />
-                  <hr />
-                </div>
-                <div className={styles.Input}>
-                  <h6>Добавьте номер телефона</h6>
-                  <input type="number" />
-                  <hr />
-                </div>
-                {activeButton === "delivery" && (
-                  <div className={styles.Input}>
-                    <h6>Укажите адрес доставки</h6>
-                    <input type="text" />
-                    <hr />
-                  </div>
-                )}
-                <div className={styles.Input}>
-                  <h6>Комментарии к заказу</h6>
-                  <input type="text" />
-                  <hr />
-                </div>
-              </div>
-              <div className={styles.uploadFile}>
-                <div className={styles.text1}>
-                  <h4>Товары: 4шт</h4>
-                  <h4>2800 сом</h4>
-                </div>
-                <div className={styles.text2}>
-                  <h4>Цену за доставку обговаривается с продавцом</h4>
-                </div>
-                <div className={styles.text3}>
-                  <h3>Итого</h3>
-                  <h3>2800 сом</h3>
-                </div>
-                <div className={styles.text4}>
-                  <h2>Загрузите чек оплаты</h2>
-                  <div className={styles.text5}>
-                    <p>Реквизиты в шапке страницы</p>
-                    <ImgStop />
-                  </div>
-                </div>
-                <div className={styles.upload}>
-                  <div className={styles.selectFile}>
-                    <input type="file" />
-                  </div>
-                  <button>Оформить заказ</button>
-                </div>
-              </div>
             </div>
-          </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default PlacinganOrder;
